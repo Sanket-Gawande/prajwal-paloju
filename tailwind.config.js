d9dd7839530b30f754eed836b19648ab5598b146
Sanket-Gawande <sanketgawande.gcoey@gmail.com>
2022-05-21 12:16:54 +0530

    final build

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        main: {
+          100: "#11263D",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
